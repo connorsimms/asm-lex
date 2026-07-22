30fbb54539801709020da1033c2dc23320241573
Connor Simms <connorvsimms@gmail.com>
2026-07-22 23:10:07 +0000

    More cursor testing

--- asm-lex/src/cursor/tests.rs
+++ asm-lex/src/cursor/tests.rs
@@ -67,6 +67,9 @@ fn test_at_line_start() {
     assert!(cursor.at_line_start());
     cursor.advance(usize::MAX);
     assert!(cursor.at_line_start());
+    let mut cursor = Cursor::new(b"\nSome\nstring");
+    cursor.advance(usize::MAX);
+    assert!(!cursor.at_line_start());
 }
 
 #[test]
@@ -82,17 +85,43 @@ fn test_peek() {
 #[test]
 fn test_seek() {
     let mut cursor = Cursor::new(b"Some string");
+    assert_eq!(cursor.seek(isize::MIN), None);
     assert_eq!(cursor.seek(-1), None);
     assert_eq!(cursor.seek(0), Some(b'S'));
     assert_eq!(cursor.seek(5), Some(b's'));
+    assert_eq!(cursor.seek(isize::MAX), None);
     cursor.restore(5);
+    assert_eq!(cursor.seek(isize::MIN), None);
     assert_eq!(cursor.seek(-5), Some(b'S'));
     assert_eq!(cursor.seek(0), Some(b's'));
     assert_eq!(cursor.seek(6), None);
+    assert_eq!(cursor.seek(isize::MAX), None);
 }
 
 #[test]
-fn test_starts_with() {}
+fn test_starts_with() {
+    let mut cursor = Cursor::new(b"Some string");
+    assert!(cursor.starts_with(b""));
+    assert!(cursor.starts_with(b"S"));
+    assert!(cursor.starts_with(b"Some"));
+    assert!(cursor.starts_with(b"Some string"));
+    assert!(cursor.starts_with(b"Some s"));
+    assert!(!cursor.starts_with(b"Somestringg"));
+    assert!(!cursor.starts_with(b"Some stringg"));
+    assert!(!cursor.starts_with(b"Some string "));
+    assert!(!cursor.starts_with(b"Some string\n"));
+    cursor.advance(usize::MAX);
+    assert!(cursor.starts_with(b""));
+    assert!(!cursor.starts_with(b"g"));
+}
+
+proptest! {
+    #[test]
+    fn prop_test_starts_with(bytes in vec(any::<u8>(), 0..100), needle in vec(any::<u8>(),0..200)) {
+        let cursor = Cursor::new(&bytes);
+        prop_assert_eq!(cursor.starts_with(needle.as_ref()), bytes.starts_with(needle.as_ref()));
+    }
+}
 
 #[test]
 fn test_bump() {
@@ -106,6 +135,39 @@ fn test_bump() {
     assert_eq!(cursor.pos(), 11);
     assert_eq!(cursor.bump(), None);
     assert_eq!(cursor.pos(), 11);
+}
+
+proptest! {
+    #[test]
+    fn prop_test_bump(bytes in vec(any::<u8>(), 0..100)) {
+        let mut cursor = Cursor::new(&bytes);
+        for _ in 0..=100 {
+            let pos = cursor.pos();
+            if cursor.bump().is_some() {
+                prop_assert_eq!(cursor.pos(), pos + 1);
+            } else {
+                prop_assert_eq!(cursor.pos(), pos);
+                prop_assert_eq!(cursor.pos(), bytes.len());
+            }
+        }
+    }
+
+    #[test]
+    fn prop_test_bump_peek(bytes in vec(any::<u8>(), 0..100)) {
+        let mut cursor = Cursor::new(&bytes);
+        for _ in 0..=100 {
+            prop_assert_eq!(cursor.peek(), cursor.bump());
+        }
+    }
+
+    #[test]
+    fn prop_test_bump_seek(bytes in vec(any::<u8>(), 0..100)) {
+        let mut cursor = Cursor::new(&bytes);
+        for _ in 0..bytes.len() {
+            prop_assert_eq!(cursor.bump(), cursor.seek(-1));
+        }
+        prop_assert_eq!(cursor.bump(), None);
+    }
 }
 
 #[test]
@@ -127,6 +189,32 @@ fn test_eat() {
     assert_eq!(cursor.pos(), 11);
 }
 
+proptest! {
+    #[test]
+    fn prop_test_eat_peek(bytes in vec(any::<u8>(), 0..100), attempts in vec(any::<u8>(), 0..200)) {
+        let mut cursor = Cursor::new(&bytes);
+        for attempt in attempts {
+            if Some(attempt) == cursor.peek() {
+                prop_assert!(cursor.eat(attempt));
+            } else {
+                prop_assert!(!cursor.eat(attempt));
+            }
+        }
+    }
+
+    #[test]
+    fn prop_test_eat_seek(bytes in vec(any::<u8>(), 0..100), attempts in vec(any::<u8>(), 0..200)) {
+        let mut cursor = Cursor::new(&bytes);
+        for attempt in attempts {
+            if cursor.eat(attempt) {
+                prop_assert_eq!(cursor.seek(-1), Some(attempt));
+            } else {
+                prop_assert_ne!(cursor.peek(), Some(attempt));
+            }
+        }
+    }
+}
+
 #[test]
 fn test_eat_while() {
     let mut cursor = Cursor::new(b"Some string");
@@ -142,7 +230,13 @@ fn test_eat_while() {
 }
 
 proptest! {
-    // #[test]
-    // fn prop_test_eat_while() {
-    // }
+    #[test]
+    fn prop_test_eat_while(bytes in vec(any::<u8>(), 0..100), set in vec(any::<u8>(), 0..100)) {
+        let mut cursor = Cursor::new(&bytes);
+        let span = cursor.eat_while(|b| set.contains(&b));
+        for b in &bytes[span] {
+            prop_assert!(set.contains(b));
+        }
+        prop_assert!(!cursor.peek().is_some_and(|b| set.contains(&b)));
+    }
 }
