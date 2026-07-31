c6aabfdfe9f5db118dae526922e1cd9d81f87872
Connor Simms <connorvsimms@gmail.com>
2026-07-31 20:13:47 +0000

    Added prop_test functions for invariants

---
+++ asm-lex/tests/proptest_gas.rs
@@ -0,0 +1,85 @@
+mod common;
+
+use asm_lex::source::gas::targets::*;
+use asm_lex::source::gas::Gas;
+use asm_lex::source::Item;
+use common::invariants;
+use common::*;
+use proptest::collection::vec;
+use proptest::prelude::*;
+use proptest::test_runner::Config;
+use proptest::test_runner::FileFailurePersistence;
+
+fn config_file(file: &'static str) -> Config {
+    Config {
+        failure_persistence: Some(Box::new(FileFailurePersistence::Direct(String::leak::<
+            'static,
+        >(format!(
+            "{}/{}/{}",
+            env!("CARGO_MANIFEST_DIR"),
+            "proptest-regressions/invariants",
+            file
+        ))))),
+        ..Config::default()
+    }
+}
+
+fn asm_byte() -> impl Strategy<Value = u8> {
+    prop_oneof![
+        20 => Just(b'\n'), 20 => Just(b' '), 10 => Just(b'\t'),
+        20 => b':'..=b'@', 30 => b'!'..=b'/', 20 => b'['..=b'`',
+        50=>b'a'..=b'z',20=>b'0'..=b'9', 10 => any::<u8>(),
+    ]
+}
+
+proptest! {
+    #![proptest_config(config_file("gas_x86_64_linux_elf.txt"))]
+
+    #[test]
+    fn x86_64_linux_elf(bytes in vec(asm_byte(), 0..1000))
+    {
+        let items: Vec<Item> = Lexer::<Gas<X86_64LinuxElf>>::from_bytes(&bytes).collect();
+        invariants::monotonic_valid_spans(&bytes, &items);
+        invariants::starts_line_iff_newline(&bytes, &items);
+        invariants::containing_item_spans(&items);
+    }
+}
+
+proptest! {
+    #![proptest_config(config_file("gas_aarch64_linux_elf.txt"))]
+
+    #[test]
+    fn aarch64_linux_elf(bytes in vec(asm_byte(), 0..1000))
+    {
+        let items: Vec<Item> = Lexer::<Gas<Aarch64LinuxElf>>::from_bytes(&bytes).collect();
+        invariants::monotonic_valid_spans(&bytes, &items);
+        invariants::starts_line_iff_newline(&bytes, &items);
+        invariants::containing_item_spans(&items);
+    }
+}
+
+proptest! {
+    #![proptest_config(config_file("gas_arm_linux_eabi_elf.txt"))]
+
+    #[test]
+    fn arm_linux_eabi_elf(bytes in vec(asm_byte(), 0..1000))
+    {
+        let items: Vec<Item> = Lexer::<Gas<ArmLinuxEabiElf>>::from_bytes(&bytes).collect();
+        invariants::monotonic_valid_spans(&bytes, &items);
+        invariants::starts_line_iff_newline(&bytes, &items);
+        invariants::containing_item_spans(&items);
+    }
+}
+
+proptest! {
+    #![proptest_config(config_file("gas_riscv64_linux_elf.txt"))]
+
+    #[test]
+    fn riscv64_linux_elf(bytes in vec(asm_byte(), 0..1000))
+    {
+        let items: Vec<Item> = Lexer::<Gas<Riscv64LinuxElf>>::from_bytes(&bytes).collect();
+        invariants::monotonic_valid_spans(&bytes, &items);
+        invariants::starts_line_iff_newline(&bytes, &items);
+        invariants::containing_item_spans(&items);
+    }
+}
