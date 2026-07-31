926ed82925b806cd8cdcefd0714877a5a65dba56
Connor Simms <connorvsimms@gmail.com>
2026-07-31 20:08:37 +0000

    Added invariant helpers

--- asm-lex/tests/common/invariants.rs
+++ asm-lex/tests/common/invariants.rs
@@ -1,12 +1,72 @@
-#[allow(unused)]
-use asm_lex::cursor::Cursor;
-#[allow(unused)]
-use asm_lex::source::Dialect;
+use super::*;
+use asm_lex::source::Item;
+use asm_lex::source::Kind;
+use asm_lex::Span;
 
+// Item spans are
+// - monotonically increasing,
+// - nonempty (start < end)
+// - contained in 0..bytes.len()
 #[allow(unused)]
-fn assert_coverage<D: Dialect>(bytes: &[u8]) {
-    let mut cursor = Cursor::new(bytes);
-    while let Some(item) = D::next_item(&mut cursor) {
-        todo!();
+pub fn monotonic_valid_spans(bytes: &[u8], items: &[Item]) {
+    let mut prev_end = 0usize;
+    for item in items {
+        let Span { start, end } = item.span();
+        assert!(start < end, "Item span {:?}..{:?} is empty", start, end);
+        assert!(
+            end <= &bytes.len(),
+            "Item span {:?}..{:?} not contained in bytes 0..{}",
+            start,
+            end,
+            bytes.len()
+        );
+        assert!(
+            *start >= prev_end,
+            "Item started at {:?} before previous Item's end at {:?}",
+            start,
+            prev_end
+        );
+        prev_end = *end;
+    }
+}
+
+// An Item starts a physical line iff it is the first item in the file OR
+// there is at least one newline between itself and the previous Item.
+#[allow(unused)]
+pub fn starts_line_iff_newline(bytes: &[u8], items: &[Item]) {
+    let mut prev_end = 0usize;
+    for item in items {
+        let Span { start, end } = item.span();
+        if prev_end == 0 {
+            assert!(item.starts_line(), "First item must start line");
+        } else if item.starts_line() {
+            assert!(
+                bytes[prev_end..*start].contains(&b'\n'),
+                "starts_line = true but no newline found in {:?}",
+                prev_end..*start
+            );
+        } else if !item.starts_line() {
+            assert!(
+                !bytes[prev_end..*start].contains(&b'\n'),
+                "starts_line = false but newline found in {:?}",
+                prev_end..*start
+            );
+        }
+        prev_end = *end;
+    }
+}
+
+// An Item's span must completely contain the subspans of its member `kind`
+#[allow(unused)]
+pub fn containing_item_spans(items: &[Item]) {
+    use Kind::*;
+    for item in items {
+        let span = item.span();
+        for_each_span(item.kind(), |field, sub_span| {
+            assert!(
+                span.start <= sub_span.start && sub_span.end <= span.end,
+                "Item span {span:?} does not contain {field:?} span {sub_span:?}"
+            )
+        });
     }
 }
