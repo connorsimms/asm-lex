5f0ea6e6be44a1d82d084f7670501b515ccdd6a3
Connor Simms <connorvsimms@gmail.com>
2026-08-10 16:24:09 -0500

    Rename targets

--- asm-lex/src/source/llvm/targets.rs
+++ asm-lex/src/source/llvm/targets.rs
@@ -1,17 +1,17 @@
 use super::LlvmTarget;
 
 // not sure if <arch><os><obj> is ideal
-pub struct X86LinuxElf {}
+pub struct X86Elf {}
 pub struct X86Darwin {}
-pub struct ArmLinuxElf {}
+pub struct ArmElf {}
 pub struct ArmDarwin {}
-pub struct Aarch64LinuxElf {}
+pub struct Aarch64Elf {}
 pub struct Aarch64Darwin {}
-pub struct RiscvLinuxElf {}
+pub struct RiscvElf {}
 pub struct RiscvDarwin {}
 
 // https://github.com/llvm/llvm-project/blob/main/llvm/lib/Target/X86/MCTargetDesc/X86MCAsmInfo.cpp
-impl LlvmTarget for X86LinuxElf {
+impl LlvmTarget for X86Elf {
     const USE_AT_FOR_SPECIFIER: bool = true;
 }
 impl LlvmTarget for X86Darwin {
@@ -20,7 +20,7 @@ impl LlvmTarget for X86Darwin {
 }
 
 // https://github.com/llvm/llvm-project/blob/main/llvm/lib/Target/ARM/MCTargetDesc/ARMMCAsmInfo.cpp
-impl LlvmTarget for ArmLinuxElf {
+impl LlvmTarget for ArmElf {
     const COMMENT_STR: &'static [u8] = b"@";
     const USE_AT_FOR_SPECIFIER: bool = true;
 }
@@ -30,7 +30,7 @@ impl LlvmTarget for ArmDarwin {
 }
 
 // https://github.com/llvm/llvm-project/blob/main/llvm/lib/Target/AArch64/MCTargetDesc/AArch64MCAsmInfo.cpp
-impl LlvmTarget for Aarch64LinuxElf {
+impl LlvmTarget for Aarch64Elf {
     const COMMENT_STR: &'static [u8] = b"//";
 }
 impl LlvmTarget for Aarch64Darwin {
@@ -39,7 +39,7 @@ impl LlvmTarget for Aarch64Darwin {
 }
 
 // https://github.com/llvm/llvm-project/blob/main/llvm/lib/Target/RISCV/MCTargetDesc/RISCVMCAsmInfo.cpp
-impl LlvmTarget for RiscvLinuxElf {}
+impl LlvmTarget for RiscvElf {}
 impl LlvmTarget for RiscvDarwin {
     const SEPARATOR_STR: &'static [u8] = b"%%";
     const COMMENT_STR: &'static [u8] = b";";
