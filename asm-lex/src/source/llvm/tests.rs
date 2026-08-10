e7e785b190a9e2886ed4b1b46bc48f99be08d2ac
Connor Simms <connorvsimms@gmail.com>
2026-08-10 17:48:52 -0500

    Moooore tests [skip ci]

--- asm-lex/src/source/llvm/tests.rs
+++ asm-lex/src/source/llvm/tests.rs
@@ -281,3 +281,223 @@ fn try_line_comment_x86_darwin() {
     ];
     check_try_line_comment::<X86Darwin>(cases);
 }
+
+fn check_is_comment<T: LlvmTarget>(cases: &[(&[u8], bool)]) {
+    for (bytes, res) in cases {
+        let cursor = Cursor::new(bytes);
+        assert_eq!(Llvm::<T>::is_comment(&cursor), *res);
+    }
+}
+
+#[test]
+fn is_comment_x86_elf() {
+    let cases: &[(&[u8], bool)] = &[
+        (b"# ...", true),
+        (b"// ...", true),
+        (b"@ ...", false),
+        (b"; ...", false),
+    ];
+    check_is_comment::<X86Elf>(cases);
+}
+
+#[test]
+fn is_comment_x86_darwin() {
+    let cases: &[(&[u8], bool)] = &[
+        (b"# ...", true),
+        (b"// ...", true),
+        (b"@ ...", false),
+        (b"; ...", false),
+    ];
+    check_is_comment::<X86Darwin>(cases);
+}
+
+#[test]
+fn is_comment_arm_elf() {
+    let cases: &[(&[u8], bool)] = &[
+        (b"// ...", true),
+        (b"@ ...", true),
+        (b"# ...", false),
+        (b"; ...", false),
+    ];
+    check_is_comment::<ArmElf>(cases);
+}
+
+fn check_try_comment<T: LlvmTarget>(cases: &[(&[u8], Option<Kind>, usize)]) {
+    for (bytes, kind, pos) in cases {
+        let mut cursor = Cursor::new(bytes);
+        assert_eq!(Llvm::<T>::try_comment(&mut cursor), *kind);
+        assert_eq!(cursor.pos(), *pos);
+    }
+}
+
+#[test]
+fn try_comment_x86_elf() {
+    use Kind::Comment;
+    let cases: &[(&[u8], Option<Kind>, usize)] = &[
+        (b"# ...", Some(Comment), 5),
+        (b"# ...\n", Some(Comment), 5),
+        (b"# ...\r", Some(Comment), 5),
+        (b"// ...", Some(Comment), 6),
+        (b"// ...\n", Some(Comment), 6),
+        (b"// ...\r", Some(Comment), 6),
+        (b"@ ...", None, 0),
+        (b"; ...", None, 0),
+    ];
+    check_try_comment::<X86Elf>(cases);
+}
+
+#[test]
+fn try_comment_x86_darwin() {
+    use Kind::Comment;
+    let cases: &[(&[u8], Option<Kind>, usize)] = &[
+        (b"# ...", Some(Comment), 5),
+        (b"# ...\n", Some(Comment), 5),
+        (b"# ...\r", Some(Comment), 5),
+        (b"// ...", Some(Comment), 6),
+        (b"// ...\n", Some(Comment), 6),
+        (b"// ...\r", Some(Comment), 6),
+        (b"@ ...", None, 0),
+        (b"; ...", None, 0),
+    ];
+    check_try_comment::<X86Darwin>(cases);
+}
+
+#[test]
+fn try_comment_arm_elf() {
+    use Kind::Comment;
+    let cases: &[(&[u8], Option<Kind>, usize)] = &[
+        (b"@ ...", Some(Comment), 5),
+        (b"@ ...\n", Some(Comment), 5),
+        (b"@ ...\r", Some(Comment), 5),
+        (b"// ...", Some(Comment), 6),
+        (b"// ...\n", Some(Comment), 6),
+        (b"// ...\r", Some(Comment), 6),
+        (b"# ...", None, 0),
+        (b"# ...\n", None, 0),
+        (b"# ...\r", None, 0),
+        (b"; ...", None, 0),
+    ];
+    check_try_comment::<ArmElf>(cases);
+}
+
+fn check_is_slash_star_comment<T: LlvmTarget>(cases: &[(&[u8], bool)]) {
+    for (bytes, res) in cases {
+        let cursor = Cursor::new(bytes);
+        assert_eq!(Llvm::<T>::is_slash_star_comment(&cursor), *res);
+    }
+}
+
+#[test]
+fn is_slash_star_comment() {
+    let cases: &[(&[u8], bool)] = &[
+        (b"/* ...", true),
+        (b"/ ...", false),
+        (b"// ...", false),
+        (b"# ...", false),
+        (b"@ ...", false),
+        (b"* ...", false),
+    ];
+    check_is_slash_star_comment::<X86Elf>(cases);
+    check_is_slash_star_comment::<X86Darwin>(cases);
+    check_is_slash_star_comment::<ArmElf>(cases);
+}
+
+fn check_try_slash_star_comment<T: LlvmTarget>(cases: &[(&[u8], Option<Kind>, usize)]) {
+    for (bytes, kind, pos) in cases {
+        let mut cursor = Cursor::new(bytes);
+        assert_eq!(Llvm::<T>::try_slash_star_comment(&mut cursor), *kind);
+        assert_eq!(cursor.pos(), *pos);
+    }
+}
+
+#[test]
+fn try_slash_star_comment() {
+    use Kind::Comment;
+    let cases: &[(&[u8], Option<Kind>, usize)] = &[
+        (b"/* ... */", Some(Comment), 9),
+        (b"/* ... */ ", Some(Comment), 9),
+        (b"/* ... */\t", Some(Comment), 9),
+        (b"/* ... *//", Some(Comment), 9),
+        (b"/* ... */#", Some(Comment), 9),
+        (b"/* ... ", Some(Comment), 7),
+        (b"// ... ", None, 0),
+        (b"## ... ", None, 0),
+        (b"@@ ... ", None, 0),
+    ];
+    check_try_slash_star_comment::<X86Elf>(cases);
+    check_try_slash_star_comment::<X86Darwin>(cases);
+    check_try_slash_star_comment::<ArmElf>(cases);
+}
+
+fn check_lex_args<T: LlvmTarget>(cases: &[(&[u8], Option<Span>, usize)]) {
+    for (bytes, span, pos) in cases {
+        let mut cursor = Cursor::new(bytes);
+        assert_eq!(Llvm::<T>::lex_args(&mut cursor), *span);
+        assert_eq!(cursor.pos(), *pos);
+    }
+}
+
+#[test]
+fn try_lex_args_x86_elf_and_darwin() {
+    let cases: &[(&[u8], Option<Span>, usize)] = &[
+        (b"a b c", Some(0..5), 5),
+        (b"a b c ", Some(0..5), 5),
+        (b" a b c", Some(1..6), 6),
+        (b"a b c\n", Some(0..5), 5),
+        (b"a b c\r", Some(0..5), 5),
+        (b"a b c;", Some(0..5), 5),
+        (b"a b c#", Some(0..5), 5),
+        (b"a b c//", Some(0..5), 5),
+        (b"a / b c", Some(0..7), 7),
+        (b"'a' b c", Some(0..7), 7),
+        (b"a 'b' c", Some(0..7), 7),
+        (b"a '\n' c", Some(0..7), 7),
+        (b"\"a\" b c", Some(0..7), 7),
+        (b"a \"b\" c", Some(0..7), 7),
+        (b"a \"\n\" c", Some(0..7), 7),
+        (b"a /*...*/ b c", Some(0..13), 13),
+        (b"a b c /*...*/", Some(0..5), 5),
+        (b"", None, 0),
+        (b"/*...*/", None, 0),
+        (b"//...", None, 0),
+    ];
+    check_lex_args::<X86Elf>(cases);
+    check_lex_args::<X86Darwin>(cases);
+}
+
+#[test]
+fn try_lex_args_arm_elf_and_darwin() {
+    let cases: &[(&[u8], Option<Span>, usize)] = &[
+        (b"a b c", Some(0..5), 5),
+        (b"a b c ", Some(0..5), 5),
+        (b" a b c", Some(1..6), 6),
+        (b"a b c\n", Some(0..5), 5),
+        (b"a b c\r", Some(0..5), 5),
+        (b"a b c@", Some(0..5), 5),
+        (b"a b c;", Some(0..5), 5),
+        (b"a b c//", Some(0..5), 5),
+        (b"a # c#", Some(0..6), 6),
+        (b"a / b c", Some(0..7), 7),
+        (b"'a' b c", Some(0..7), 7),
+        (b"a 'b' c", Some(0..7), 7),
+        (b"a '\n' c", Some(0..7), 7),
+        (b"\"a\" b c", Some(0..7), 7),
+        (b"a \"b\" c", Some(0..7), 7),
+        (b"a \"\n\" c", Some(0..7), 7),
+        (b"a /*...*/ b c", Some(0..13), 13),
+        (b"a b c /*...*/", Some(0..5), 5),
+        (b"", None, 0),
+        (b"/*...*/", None, 0),
+        (b"//...", None, 0),
+    ];
+    check_lex_args::<ArmElf>(cases);
+    check_lex_args::<ArmDarwin>(cases);
+}
+
+fn check_try_symbol_kind<T: LlvmTarget>(cases: &[(&[u8], Option<Kind>, usize)]) {
+    for (bytes, kind, pos) in cases {
+        let mut cursor = Cursor::new(bytes);
+        assert_eq!(Llvm::<T>::try_symbol_kind(&mut cursor), *kind);
+        assert_eq!(cursor.pos(), *pos);
+    }
+}
